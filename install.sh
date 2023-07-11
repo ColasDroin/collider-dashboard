caf5991becbe9a20a2ec4bb57fa96cf30c899fa7
Colas <colasdroin@gmail.com>
2023-07-11 16:37:35 +0200

    Fixed installation

--- install.sh
+++ install.sh
@@ -7,8 +7,6 @@ cd xmask
 git submodule init
 git submodule update
 cd ..
-#git clone https://github.com/PyCOMPLETE/FillingPatterns.git
-#pip install -e FillingPatterns
 git clone https://github.com/ColasDroin/twiss_check.git
 git clone https://github.com/ColasDroin/build_collider.git
 cd ..
