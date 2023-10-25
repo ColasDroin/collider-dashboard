83996cd32aa708811c1836cf5f794490b24ebf0b
Colas <colasdroin@gmail.com>
2023-10-25 17:40:27 +0200

    Update gitignore

--- app/dashboard.py
+++ app/dashboard.py
@@ -43,7 +43,7 @@ path_collider = "/afs/cern.ch/work/c/cdroin/private/example_DA_study/master_stud
 # "/afs/cern.ch/work/c/cdroin/private/example_DA_study/master_study/scans/all_optics_2023/collider_20/xtrack_0000/collider.json"
 path_job = path_collider.split("/final_collider.json")[0]
 dic_without_bb, dic_with_bb, initial_pickle_path = init.init_from_collider(
-    path_collider, load_global_variables_from_pickle=False
+    path_collider, load_global_variables_from_pickle=True
 )
 
 # Activating this will allow to select a collider from the dropdown menu, but will restrict the choice to preloaded colliders
