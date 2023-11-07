12149844175a15631cc33e27071eadfd935b7f7a
Colas <colasdroin@gmail.com>
2023-11-07 10:16:10 +0100

    Cleaning poetry installation

--- dashboard/__main__.py
+++ dashboard/__main__.py
@@ -3,7 +3,7 @@
 # ==================================================================================================
 # --- Imports
 # ==================================================================================================
-import sys
+import argparse
 
 from .dashboard import build_app
 
@@ -11,17 +11,25 @@ from .dashboard import build_app
 # ==================================================================================================
 # --- Launch app
 # ==================================================================================================
-def main(path_collider):
+def main(path_collider, port):
     app, _ = build_app(path_collider)  # server not needed for local deployment
-    app.run_server(debug=False, host="0.0.0.0", port=8080)
+    app.run_server(debug=False, host="0.0.0.0", port=port)
 
 
 if __name__ == "__main__":
     # Get the collider path from the command line
-    if len(sys.argv) == 2:
-        path_collider = sys.argv[1]
-    else:
-        print("No collider path was provided. Launching example collider.")
-        path_collider = None
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-path", help="path to the collider")
+    parser.add_argument("-port", help="port to run the dashboard on")
+    args = parser.parse_args()
+    path_collider = args.path
+    port = args.port
 
-    main(path_collider)
+    if path_collider is None:
+        print("No collider path was provided. Launching example collider.")
+        path_collider = "dashboard/data/collider.json"
+    if port is None:
+        print("No port was provided. Launching on port 8080.")
+        port = 8080
+
+    main(path_collider, port)
--- docs/index.md
+++ docs/index.md
@@ -15,10 +15,16 @@ This will install the required packages and build the application.
 
 ## Usage
 
-The simplest way to use the dashboard is to run the package from the command line, providing the path of a collider json file:
+For personal usage, the simplest way to use the dashboard is to run the package from the command line, providing the path of a collider json file:
 
 ```bash
 python -m simulation-dashboard path_to_collider.json
+```
+
+If needed, one can specify the port on which the dashboard will be deployed:
+
+```bash
+python -m simulation-dashboard path_to_collider.json --port 8080
 ```
 
 After computing some temporary variables (this may take a while the first time), this will deploy a local server and open the dashboard in a browser window.
@@ -29,16 +35,17 @@ Alternatively, one can import the dashboard as a module and use it in a custom s
 # my-awesome-dashboard.py
 
 from simulation_dashboard import build_app
-app, server = build_app(path_to_collider.json)
+app, server = build_app(path_to_collider.json, port=8080)
 ```
 
-The dashboard can then be deployed on a remote server, e.g. with gunicorn (here on port 8080):
+The dashboard can then be deployed on a remote server, e.g. with gunicorn:
 
 ```bash
 gunicorn my-awesome-dashboard:server -b :8080
 ```
 
 Note that, as the dashboard deals with global variables, it is not thread-safe. It is therefore recommended to run it with a single worker (it's the case by default).
+
 
 ## Project repository
 
